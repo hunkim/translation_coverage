9f3fa71b577a0eb33fc619d1a61ad5fd8cfc9a18
Sung Kim <hunkim@gmail.com>
2016-10-12 08:52:15 +0800

    Added a strong output test

--- tests/testMain.py
+++ tests/testMain.py
@@ -46,11 +46,19 @@ class TestUtilsMethods(unittest.TestCase):
 
     def test_trans_coverage(self):
         args = main.parse_args(["--dir=tests"])
+        ext = tuple(args.ext.split())
 
-        e_count, n_count = main.trans_coverage(0, args, "tests")
+        e_count, n_count = main.trans_coverage(-1, args, "tests", ext)
         print("test dir: ", e_count, n_count)
         self.assertNotEqual(0, e_count)
         self.assertNotEqual(0, n_count)
+
+        # Generate/update sample file
+        # `python main.py --dir tests > sample.md`
+        with open("sample.md", "r") as sample:
+            s1 = sample.read().splitlines()
+            s2 = main.print_out(args).splitlines()
+            self.assertEqual(s1, s2)
 
 if __name__ == '__main__':
     unittest.main()
